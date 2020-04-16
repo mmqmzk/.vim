af20ff2d944577b98206b37bc64cd250969e574a
mmqmzk <mmqmzk@gmail.com>
2020-04-16 10:41:14 +0800

    remove unused

--- install.sh
+++ install.sh
@@ -5,11 +5,4 @@ set -e
 pushd "$(dirname "$0")"
 git submodule update --init
 vim +PlugInstall +PlugUpdate +qa
-pip3 install --user pynvim -U
-npm install -g bash-language-server
-if which brew &> /dev/null; then
-  brew install shellcheck shfmt
-else
-  sudo apt update -qq && sudo apt install shellcheck -y
-fi
 popd
